1b327bcbafaea7072050c9b50638d034fcbce70b
andrlone <andrlone@stud.ntnu.no>
2017-04-06 14:03:32 +0200

    fixed value generation and normalisation

--- MasterOppgave/MasterOppgave/Genome.cs
+++ MasterOppgave/MasterOppgave/Genome.cs
@@ -13,38 +13,48 @@ namespace MasterOppgave
         // Stimulation    =   1
         // Hedonism       =   2
         // Achievement    =   3
-        // Power          =   4
+        // Power          =   4      
         // Security       =   5
         // Conformity     =   6
         // Tradition      =   7
         // Benevolence    =   8
         // Univarsalism   =   9
+        // groups: 0,1,2 - 2,3,4 - 5,6,7 - 8,9
+        // opposites: 0,5 - 1,5 - 2,67 - 3,8 - 4,9
         private List<double> genomeValues;
         private List<double> genomeNormalised;
 
         public Genome(List<double> genome)
         {
             genomeValues = genome;
-            genomeNormalised = normalise(genomeValues);
+            genomeNormalised = new List<double>(10);
         }
         public Genome()
         {
             // TODO: verdier skal ha et forhold til hverandre
             Random rng = new Random();
-            genomeValues = new List<double>();
-            for(int i=0; i<10; i++)
-            {
-                genomeValues.Add(rng.Next(0, 100));
-            }
+            genomeValues = new List<double>(10);
+            int n = 20;
 
-            genomeNormalised = normalise(genomeValues);
+            genomeValues[0] = rng.Next(0,100); // Random value
+            genomeValues[1] = genomeValues[0] * (rng.Next(0, n) / 100); // Close to value #0
+            genomeValues[5] = (100 - genomeValues[0]) * (rng.Next(0, n) / 100); // Opposite of #0 
+            genomeValues[6] = genomeValues[5] * (rng.Next(0, n) / 100); // Close to value #5
+            genomeValues[7] = genomeValues[6] * (rng.Next(0, n) / 100); // Close to value #6
+
+            genomeValues[3] = rng.Next(0, 100); // Random value
+            genomeValues[4] = genomeValues[3] * (rng.Next(0, n) / 100); // Close to value #3
+            genomeValues[8] = (100 - genomeValues[4]) * (rng.Next(0, n) / 100); // Opposite of #4
+            genomeValues[9] = genomeValues[8] * (rng.Next(0, n) / 100);
+            
+            genomeValues[2] = (genomeValues[1]+genomeValues[3] / 2) * (rng.Next(0, n/2) / 100); // Close to #3 and #1
+
+            normalise(genomeValues);
         }
 
-        public List<double> normalise(List<double> genome)
+        private void normalise(List<double> genome)
         {
-            //List<double> normalised = genome.Select(i => i * (100.0/genome.Max()) ).ToList();
-            //return normalised;
-            return genome.Select(i => i * (100.0/genome.Max()) ).ToList();
+            genomeNormalised = genome.Select(i => i * (100.0/genome.Max()) ).ToList();
         }
 
         public void mutate(double p)
